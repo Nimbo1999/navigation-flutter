22958200bede45187003359289cce5178d9300ff
Matheus de Oliveira Lopes <matlopes1999@gmail.com>
2020-02-14 10:32:48 -0300

    Adicionando as Categorias.

---
+++ lib/pages/categories.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+import '../data/dummy-data.dart';
+import '../widgets/category_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Categories'),
+      ),
+      body: GridView(
+        children: DUMMY_CATEGORIES.map(
+          (category) => 
+            CategoryItem(
+              category.title,
+              category.color
+            )
+        ).toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20 
+        ),
+      ),
+    );
+  }
+}
