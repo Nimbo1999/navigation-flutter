40e39d39e3dc566cc74a2fe3782ba0fed475a309
Matheus de Oliveira Lopes <matlopes1999@gmail.com>
2020-02-14 10:57:45 -0300

    Add Theme configurations

--- lib/pages/categories.dart
+++ lib/pages/categories.dart
@@ -11,6 +11,7 @@ class CategoriesScreen extends StatelessWidget {
         title: const Text('Categories'),
       ),
       body: GridView(
+        padding: const EdgeInsets.all(20),
         children: DUMMY_CATEGORIES.map(
           (category) => 
             CategoryItem(
